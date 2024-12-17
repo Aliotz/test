52cf7ba1d0b8a3e28641e023143a07a0af12d900
Aliotz <alihosseinzade361@gmail.com>
2024-12-17 21:44:06 +0330

    Pascal
    this is pascal function to create row who we want.

---
+++ pascal_function.py
@@ -0,0 +1,12 @@
+
+'''creted by SeyedAli-HosseinZade and sajjed Dalili'''
+
+def pascal(n):
+    t = [1]
+    y = [0]
+    for _ in range(n):
+        print(t)
+        t =[i+j  for i,j in zip(t+y , y+t)]
+
+row = int(input('please enter the pascal row you want:'))
+pascal(row)
